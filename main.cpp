e5eed6b4a8a61f0bd0ff9f4fca398ca883a6a8b8
otherdelusions <ilyaisstupid@gmail.com>
2025-03-12 01:46:15 +0300

    im tired boss

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <vector>
 #include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 class Monom {
 public:
@@ -10,37 +10,37 @@ public:
 
     Monom(double coef = 0, int pow = 0) : c(coef), q(pow) {}
 
-    Monom operator+(const Monom& other) {
+    Monom operator+(const Monom &other) {
         if (q != other.q) {
             throw std::invalid_argument("power mismatch");
         }
-        return Monom(c + other.c, q);   
+        return Monom(c + other.c, q);
     }
 
-    Monom operator-(const Monom& other) {
+    Monom operator-(const Monom &other) {
         if (q != other.q) {
             throw std::invalid_argument("power mismatch");
         }
         return Monom(c - other.c, q);
     }
 
-    Monom operator*(const Monom& other) {
+    Monom operator*(const Monom &other) {
         return Monom(c * other.c, q + other.q);
     }
 
-    Monom operator/(const Monom& other) {
+    Monom operator/(const Monom &other) const {
         if (other.c == 0) {
             throw std::invalid_argument("cant divide by 0");
         }
         return Monom(c / other.c, q - other.q);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Monom& m) {
+    friend std::ostream &operator<<(std::ostream &os, const Monom &m) {
         os << m.c << "x^" << m.q;
         return os;
     }
 
-    friend std::istream& operator>>(std::istream& is, Monom& m) {
+    friend std::istream &operator>>(std::istream &is, Monom &m) {
         is >> m.c >> m.q;
         return is;
     }
@@ -51,8 +51,8 @@ private:
     std::vector<Monom> monoms;
 
 public:
-    void addMonom(const Monom& newm) {
-        for (auto& monom : monoms) {
+    void addMonom(const Monom &newm) {
+        for (auto &monom : monoms) {
             if (monom.q == newm.q) {
                 monom = monom + newm;
                 return;
@@ -61,23 +61,23 @@ public:
         monoms.push_back(newm);
     }
 
-    Polynomial operator+(const Polynomial& other) {
+    Polynomial operator+(const Polynomial &other) {
         Polynomial result = *this;
-        for (const auto& m : other.monoms) {
+        for (const auto &m : other.monoms) {
             result.addMonom(m);
         }
         return result;
     }
 
-    Polynomial operator-(const Polynomial& other) {
+    Polynomial operator-(const Polynomial &other) {
         Polynomial result = *this;
-        for (const auto& m : other.monoms) {
+        for (const auto &m : other.monoms) {
             result.addMonom(Monom(-m.c, m.q));
         }
         return result;
     }
 
-    Polynomial operator*(const Polynomial& other) {
+    Polynomial operator*(const Polynomial &other) {
         Polynomial result;
         for (int i = 0; i < monoms.size(); i++) {
             for (int j = 0; j < other.monoms.size(); j++) {
@@ -88,34 +88,10 @@ public:
     }
 
     Polynomial operator/(const Polynomial& other) const {
-        if (other.monoms.empty()) {
-            throw std::invalid_argument("cant divide by 0");
-        }
-    
-        Polynomial result;
-        Polynomial remainder = *this;
-    
-        while (!remainder.monoms.empty()) {
-            Monom leadingMonom = remainder.monoms.back();
-            Monom divisorMonom = other.monoms.back();
-    
-            if (leadingMonom.q < divisorMonom.q) {
-                break; // Cannot divide further
-            }
-    
-            Monom term = leadingMonom / divisorMonom;
-            result.addMonom(term);
-    
-            Polynomial termPoly;
-            termPoly.addMonom(term);
-    
-            remainder = remainder - (termPoly * other);
-        }
-    
-        return result;
+        // TODO: Сделать деление
     }
 
-    Polynomial operator/(const Monom& m) {
+    Polynomial operator/(const Monom &m) {
         Polynomial result;
         for (int i = 0; i < monoms.size(); i++) {
             result.addMonom(monoms[i] / m);
@@ -123,7 +99,7 @@ public:
         return result;
     }
 
-    Polynomial operator*(const Monom& m) {
+    Polynomial operator*(const Monom &m) {
         Polynomial result;
         for (int i = 0; i < monoms.size(); i++) {
             result.addMonom(monoms[i] * m);
@@ -131,12 +107,10 @@ public:
         return result;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, Polynomial& p) {
-        std::sort(p.monoms.begin(), p.monoms.end(), [](const Monom& a, const Monom& b) {
-            return a.q < b.q;
-        });
-        for (int i = 0; i < p.monoms.size(); i++) {\
-            // TODO: Переделать этот стыд
+    friend std::ostream &operator<<(std::ostream &os, Polynomial &p) {
+        std::sort(p.monoms.begin(), p.monoms.end(),
+                  [](const Monom &a, const Monom &b) { return a.q < b.q; });
+        for (int i = 0; i < p.monoms.size(); i++) {  // TODO: Переделать этот стыд
             if (p.monoms[i].c == 0) {
                 os << "0";
 
@@ -145,7 +119,7 @@ public:
                 }
                 continue;
             }
-            
+
             if (p.monoms[i].c != 1 && p.monoms[i].c != -1) {
                 os << p.monoms[i].c;
             }
@@ -161,7 +135,7 @@ public:
             if (p.monoms[i].q == 1) {
                 os << "x";
             }
-            
+
             if (i < p.monoms.size() - 1) {
                 os << " + ";
             }
@@ -169,31 +143,32 @@ public:
         return os;
     }
 
-    friend std::istream& operator>>(std::istream& is, Polynomial& p) {
+    friend std::istream &operator>>(std::istream &is, Polynomial &p) {
         std::string line;
         std::getline(is, line);
-        if (line.empty()) return is;
+        if (line.empty())
+            return is;
 
         std::istringstream iss(line);
         double coef;
         int power;
-        
+
         while (iss >> coef >> power) {
             p.addMonom(Monom(coef, power));
         }
-        
+
         return is;
     }
 };
 
 int main() {
     // 5x^6 + 3x^6 + 2x^0 + 48x^3 + 9x^9 + 6x^12
-    // 5x^5 + 50x^50 + 20x^0 + 2x^6 
+    // 5x^5 + 50x^50 + 20x^0 + 2x^6
     Polynomial p1, p2;
-    std::cout << "Enter first polynomial monoms separated by space:\n";
+    std::cout << "Enter 1st polynomial's monoms separating all variables by space (e.g. 1 2 3 4 => x^2 + 3x^4):\n";
     std::cin >> p1;
 
-    std::cout << "Enter second polynomial monoms separated by space:\n";
+    std::cout << "Enter 2nd polynomial's monoms:\n";
     std::cin >> p2;
 
     std::cout << "First polynomial: " << p1 << std::endl;
@@ -202,11 +177,11 @@ int main() {
     Polynomial sum = p1 + p2;
     Polynomial diff = p1 - p2;
     Polynomial mult = p1 * p2;
-    //Polynomial div = p1 / p2;
+    Polynomial div = p1 / p2;
     std::cout << "p1 + p2: " << sum << std::endl;
     std::cout << "p1 - p2: " << diff << std::endl;
     std::cout << "p1 * p2: " << mult << std::endl;
-    //std::cout << "p1 / p2: " << div << std::endl;
+    std::cout << "p1 / p2: " << div << std::endl;
 
     std::cout << std::endl;
     Monom m(2, 6);
